ffcbbdfab53281c8bceebf97f535f3bc2245ffda
udugam <ryan.udugam@gmail.com>
2018-06-01 22:56:02 -0400

    started javascript pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// Declare 90sTvShows object
+
+    //Variables
+    //Declare shows array
+    //Declare show variable
+    //Declare wins variable and set to 0
+    //Declare guesses variable and set to 5
+    //Declare guessHistory array
+    //Declare guess variable
+    
+    //Functions
+    //Declare selectShow method
+        //randomly generate an index number i based on the length of the global "shows" array
+        //show = shows[i];
+    
+    //Declare guessCompare method that takes the guess variable as a parameter
+        //If the guess has been guessed before (check using using guessHistory.includes(guess)) 
+            //end the guess compare method and alert the user that the letter has been guessed already.
+        //Else, 
+            //add the guess to the guessHistory array using guessHistory.push(guess).
+            //decrement the guesses variable by 1
+            //if the guess letter matches one of the show letter (use show.includes(guess) or a similar method for strings)
+                //return true
+            //else 
+                //return false
+    
+    
+
